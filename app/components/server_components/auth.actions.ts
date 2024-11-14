f479ec5b07d37e9f7eba0dee6c854003c719b17a
Steven Castro <castrostevenyc@gmail.com>
2024-11-13 23:13:05 -0500

    update signout

--- app/components/server_components/auth.actions.ts
+++ app/components/server_components/auth.actions.ts
@@ -1,33 +1,7 @@
 'use server'
-import querystring from 'querystring';
-import { signIn, signOut } from "@/lib/auth"
-import { redirect } from 'next/navigation';
+import { signIn } from "@/lib/auth"
 
 export const signInAction = async () => {
     await signIn("cognito", { redirectTo: "/" })
 }
 
-const generateSignOutUrl = () => {
-    const domain = "vitrav.auth.us-east-1.amazoncognito.com"; // Replace with your Cognito domain
-    const clientId = '53kqgchkbunqgfie1f5gs6qmdi' // Replace with your actual client ID
-    const redirectUri = window.location.host; // Replace with your actual redirect URI
-
-    const params = {
-        response_type: "code",
-        client_id: clientId,
-        redirect_uri: redirectUri,
-        state: "example-state-value", // You may want to generate a unique state value per request
-        nonce: "example-nonce-value", // Use a unique nonce value per request for security
-        scope: "openid profile aws.cognito.signin.user.admin",
-        logout_uri: window.location.host
-    };
-
-    const queryString = querystring.stringify(params);
-    return `https://${domain}/logout?${queryString}`;
-};
-
-export const signOutAction = async () => {
-    const logoutUrl = generateSignOutUrl();
-    await signOut()
-    redirect(logoutUrl)
-}
--- lib/signout.ts
+++ lib/signout.ts
@@ -4,7 +4,7 @@ import querystring from 'querystring';
 const generateSignOutUrl = () => {
     const domain = "vitrav.auth.us-east-1.amazoncognito.com"; // Replace with your Cognito domain
     const clientId = '53kqgchkbunqgfie1f5gs6qmdi' // Replace with your actual client ID
-    const redirectUri = "http://localhost:3000"; // Replace with your actual redirect URI
+    const redirectUri = window.location.host; // Replace with your actual redirect URI
 
     const params = {
         response_type: "code",
@@ -13,7 +13,7 @@ const generateSignOutUrl = () => {
         state: "example-state-value", // You may want to generate a unique state value per request
         nonce: "example-nonce-value", // Use a unique nonce value per request for security
         scope: "openid profile aws.cognito.signin.user.admin",
-        logout_uri: 'http://localhost:3000'
+        logout_uri: window.location.host
     };
 
     const queryString = querystring.stringify(params);
